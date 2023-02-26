ccac9536b64e6190877925bead1217ec58257db7
Emils Bernhards <emilsbernhards2001@gmail.com>
2023-02-26 20:11:29 +0100

    Add installed app deletion

--- index.ts
+++ index.ts
@@ -27,6 +27,9 @@ const developClient = contentful.createClient({
   },
 });
 
+/**
+ * Checks whether an environment with id "develop" exists in Contentful.
+ */
 const developExists = async (client: contentful.PlainClientAPI): Promise<boolean> => {
   try {
     await client.environment.get({
@@ -41,26 +44,46 @@ const developExists = async (client: contentful.PlainClientAPI): Promise<boolean
 };
 
 const deleteEverything = async (client: contentful.PlainClientAPI) => {
+  // Delete entries
   const entries = await client.entry.getMany({});
-
   for (const entry of entries.items) {
     await client.entry.unpublish({ entryId: entry.sys.id });
     await client.entry.delete({ entryId: entry.sys.id });
   }
 
+  // Delete assets
   const assets = await client.asset.getMany({});
-  
   for (const asset of assets.items) {
     await client.asset.unpublish({ assetId: asset.sys.id });
     await client.asset.delete({ assetId: asset.sys.id });
   }
   
-
+  // Delete content types
   const contentTypes = await client.contentType.getMany({});
-
   for (const contentType of contentTypes.items) {
     await client.contentType.unpublish({ contentTypeId: contentType.sys.id });
     await client.contentType.delete({ contentTypeId: contentType.sys.id });
+  }
+
+  // Delete apps
+  const appsRes = await fetch(`https://api.contentful.com/spaces/${spaceId}/environments/${masterEnvironmentId}/app_installations`, {
+    headers: {
+      'authorization': `Bearer ${accessToken}`,
+    }
+  });
+  const apps = await appsRes.json();
+
+  if (apps?.includes?.ResolvedAppDefinition) {
+    for (const app of apps?.includes?.ResolvedAppDefinition) {
+      const appId = app.sys.id;
+
+      await fetch(`https://api.contentful.com/spaces/${spaceId}/environments/${masterEnvironmentId}/app_installations/${appId}`, {
+        method: 'DELETE',
+        headers: {
+          'authorization': `Bearer ${accessToken}`,
+        }
+      });
+    }
   }
 };
 
