66d89bec64584016885a85805bb8020cad59f366
Emils Bernhards <emilsbernhards2001@gmail.com>
2023-03-06 09:26:28 +0100

    Add semicolons

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
 import * as contentful from 'contentful-management';
 
 const accessToken = process.env.MANAGEMENT_TOKEN;
